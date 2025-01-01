82d3ca7af34701022828536fb728fa3a8fbdcc79
ostanton <114495956+ostanton@users.noreply.github.com>
2025-01-01 17:08:44 +0000

    Default return values

--- diabolical_delegates.cpp
+++ diabolical_delegates.cpp
@@ -42,7 +42,7 @@ export namespace ostanton
          * @tparam RetT Return type
          * @tparam ArgsT Argument types
          */
-        template<typename C, typename RetT, typename... ArgsT>
+        template<typename C, typename RetT = void, typename... ArgsT>
         requires std::is_class_v<C>
         class CMemberDelegate final : public IDelegate<RetT, ArgsT...>
         {
@@ -90,7 +90,7 @@ export namespace ostanton
          * @tparam RetT Return type
          * @tparam ArgsT Argument types
          */
-        template<typename RetT, typename... ArgsT>
+        template<typename RetT = void, typename... ArgsT>
         class CFunctorDelegate final : public IDelegate<RetT, ArgsT...>
         {
         public:
@@ -133,7 +133,7 @@ export namespace ostanton
          * @tparam RetT Return type
          * @tparam ArgsT Argument types
          */
-        template<typename RetT, typename... ArgsT>
+        template<typename RetT = void, typename... ArgsT>
         class CEventWithReturn final
         {
         public:
@@ -191,7 +191,7 @@ export namespace ostanton
          * @tparam RetT Return type
          * @tparam ArgsT Argument types
          */
-        template<typename RetT, typename... ArgsT>
+        template<typename RetT = void, typename... ArgsT>
         class CMultiEventWithReturn final
         {
         public:
